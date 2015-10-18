349b218afb41fe105b5fdbc87c15ccd3b5a3f156
Abhijeet Deshpande <avdd80@gmail.com>
2015-10-17 18:34:23 -0700

    add common.h

---
+++ common.h
@@ -0,0 +1,5 @@
+#define CARRIER_FREQUENCY_f   150
+#define SAMPLING_FREQUENCY_Fs 4800
+#define SYMBOLS_PER_SUBFRAME  15
+#define SAMPLE_SEPARATION_Ts  208 /* microseconds */
+#define SAMPLES_PER_SYMBOL    32
