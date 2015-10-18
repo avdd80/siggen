a00f66678fa459dc6ea708b318704841b9e1a4a1
Abhijeet Deshpande <avdd80@gmail.com>
2015-10-17 22:13:56 -0700

    try2

--- qpsk.c
+++ qpsk.c
@@ -44,13 +44,10 @@ void gen_qpsk_symbols (int two_bit_data_packet);
 void generate_basis_symbol (int number_of_samples, int* symbol_ptr, int phase)
 {
 	int sample_index = (int) (phase * SAMPLES_PER_SYMBOL / 360);
-	printf ("phase = %d, SAMPLES_PER_SYMBOL = %d\n", phase, SAMPLES_PER_SYMBOL);
-	
-	//printf ("\nEntered basis sym num_samples=%d, cos[%d] = %d", number_of_samples, cosine_wave [sample_index], sample_index);
+
 	while (number_of_samples)
 	{
 		*symbol_ptr = cosine_wave [sample_index];
-		printf ("\ncos[%d] = %d", sample_index, *symbol_ptr);
 		sample_index++;
 		sample_index %= 32;
 		symbol_ptr++;
@@ -71,7 +68,7 @@ void gen_qpsk_symbols (int two_bit_data_packet)
 	/* Generate sine and cosine basis symbols */
 	//generate_basis_symbol (15*32, cosine_basis_symbol, 0);
 	generate_basis_symbol (SAMPLES_PER_SUBFRAME, sine_basis_symbol, 270);
-	exit (1);
+	printf ("\nqpsk_1pi_4\n");
 	for (i = 0; i < SAMPLES_PER_SUBFRAME; i++)
 	{
 		qpsk_1pi_4[i] = (sine_basis_symbol[i]   + cosine_basis_symbol[i]) >> 1;
@@ -79,7 +76,22 @@ void gen_qpsk_symbols (int two_bit_data_packet)
 		qpsk_5pi_4[i] = 1023 - qpsk_1pi_4[i];
 		qpsk_7pi_4[i] = 1023 - qpsk_3pi_4[i];
 		
+		printf ("\n%d", qpsk_1pi_4[i]);
+	}
+	printf ("\nqpsk_3pi_4\n");
+	for (i = 0; i < SAMPLES_PER_SUBFRAME; i++)
+	{
 		printf ("\n%d", qpsk_3pi_4[i]);
+	}
+	printf ("\nqpsk_5pi_4\n");
+	for (i = 0; i < SAMPLES_PER_SUBFRAME; i++)
+	{
+		printf ("\n%d", qpsk_5pi_4[i]);
+	}
+	printf ("\nqpsk_7pi_4\n");
+	for (i = 0; i < SAMPLES_PER_SUBFRAME; i++)
+	{
+		printf ("\n%d", qpsk_7pi_4[i]);
 	}
 	
 	return;
